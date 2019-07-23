8f653b19b09253dcfe5c83dc4d74c46a8e364947
12445 <1244560904@qq.com>
2019-07-23 14:27:48 +0800

    first commit to github

---
+++ src/main/java/com/chs/jenkinsspringbootdemo/JenkinsSpringbootDemoApplication.java
@@ -0,0 +1,22 @@
+package com.chs.jenkinsspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringbootDemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "jenkinsSpringbootDemo",method = RequestMethod.GET)
+    public String jenkinsSpringbootDemo(){
+        return "hello";
+    }
+
+}
---
+++ src/test/java/com/chs/jenkinsspringbootdemo/JenkinsSpringbootDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chs.jenkinsspringbootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsSpringbootDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
