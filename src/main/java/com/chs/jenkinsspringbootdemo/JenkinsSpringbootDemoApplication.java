494318a21100431972b72b5435b4e36b58a71e36
12445 <1244560904@qq.com>
2019-07-23 21:45:00 +0800

    "test5"

--- src/main/java/com/chs/jenkinsspringbootdemo/JenkinsSpringbootDemoApplication.java
+++ src/main/java/com/chs/jenkinsspringbootdemo/JenkinsSpringbootDemoApplication.java
@@ -16,7 +16,7 @@ public class JenkinsSpringbootDemoApplication {
 
     @RequestMapping(value = "jenkinsSpringbootDemo",method = RequestMethod.GET)
     public String jenkinsSpringbootDemo(){
-        return "get hello";
+        return "get hello webhook";
     }
 
 }
