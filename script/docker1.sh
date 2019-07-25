eaab44d8660ac10b244c7773402a2bc633e98cf7
12445 <1244560904@qq.com>
2019-07-25 09:14:05 +0800

    "test5-day3 test1"

--- script/docker1.sh
+++ script/docker1.sh
@@ -1,6 +1,5 @@
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-echo NAME
+echo $NAME
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-echo VERSION
+echo $VERSION
 java -jar target/${NAME}-${VERSION}.jar
